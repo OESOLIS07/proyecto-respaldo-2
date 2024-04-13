e3b64b8a72bc268be868c8c0e8b73df517147894
OESOLIS07 <157768093+OESOLIS07@users.noreply.github.com>
2024-04-12 23:32:59 -0600

    avance-de-prueba

---
+++ proyecto-grupo4-progra-web/back-end/servidor.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3008;
+const { Sequelize, DataTypes } = require("sequelize");
+
+app.use(express.json());
+app.use(cors());
+
+//Conecxion a Base De Datos.
+const sequelize = new Sequelize({
+    dialect: "mysql",
+    host: " ",
+    username: " ",
+    password: " ",
+    database: " ",
+});
+
+//Clase entidad para la creacion de las tablas.
+class Entity {
+    constructor(name, fields) {
+      this.name = name;
+      this.model = sequelize.define(name, fields);
+    }
+  
+    async sync() {
+      await this.model.sync({ force: true });
+      console.log(`Table for ${this.name} synchronized`);
+    }
+}
+
+//Esquema para la tabla de entidad Usuario
+const usuarioSchema = {
+    usuario_Id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    usuario_Nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+    },
+    usuario_Email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    usuario_Password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    usuario_Telefono: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+    },
+};
+
+//Crea la entidad Usuario.
+const User = new Entity("Usuario", usuarioSchema);
+
+//Sincroniza la Base de Datos.
+const syncronizeDB = () => {
+    sequelize
+      .sync()
+      .then(async () => {
+        await User.sync();
+        await Note.sync();
+      })
+      .catch((error) => {
+        console.error("Error synchronizing database:", error);
+      });
+};
+
+//nodemon servidor.js
+
+//node servidor.js
---
+++ proyecto-grupo4-progra-web/front-end/tienda-online/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HOLA, A VER QUE PASA CON ESTO JJJ</h1>
+    </div>
+  );
+}
+
+export default App;
