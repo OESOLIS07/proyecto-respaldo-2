590b658a7e3564f3b6aecd4074ed91aeb78ef19e
orlrod5 <157768122+orlrod5@users.noreply.github.com>
2024-04-14 12:33:30 -0600

    bases del front

--- proyecto-grupo4-progra-web/front-end/tienda-online/src/App.js
+++ proyecto-grupo4-progra-web/front-end/tienda-online/src/App.js
@@ -1,18 +1,27 @@
 //import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import axios from "axios";
 import Usuario from "./components/Usuario/Usuario";
-import AppBar from '@mui/material/AppBar';
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
 import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  // Función para agregar un producto al carrito
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
@@ -30,14 +39,21 @@ function App() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Tienda OnLine
             </Typography>
-            <Button color="inherit" href="/Usuario">REGISTRO</Button>
+            {/* Botón para ir a la página de Inicio */}
+            <Button color="inherit" to="/" >Inicio</Button>
+            {/* Botón para ir a la página de Registro */}
+            <Button color="inherit" to="/Usuario">Registro</Button>
+          {/* Botón para ir a la página de Carrito */}
+          <Button color="inherit" to="/cart">Carrito</Button>
           </Toolbar>
         </AppBar>
       </Box>
   
       <Router>
         <Routes>
-          <Route path="/Usuario" element={<Usuario/>} />
+          <Route path="/Usuario" element={<Usuario />} />
+          <Route path="/cart" element={<Cart cart={cart} />} />
+          <Route path="/" element={<Home addToCart={addToCart} />} />
         </Routes>
       </Router>
     </div>
